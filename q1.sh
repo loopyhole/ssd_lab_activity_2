31fa38f3e20febebccbbd7690e69ce02e077762a
ilfenomeno <naaperuamitabh@gmail.com>
2022-08-11 16:31:27 +0530

    changed wc to awk

--- q1.sh
+++ q1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 file=$1
-x=$(wc -l "$file")
-loc=$((${x:0:1}/2))
-echo | awk -v var="$loc" 'NR==var {print $0}' $file
+num=$(awk 'END {print NR}' $file)
+num=$((num/2))
+echo | awk -v var=$num 'NR==var {print $0}' $file
